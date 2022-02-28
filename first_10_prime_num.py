542b1dc9489545bd95e9882ff192b76559ea58be
Akash <chourasiaakash13@gmail.com>
2022-02-28 17:35:45 -0500

    commit

--- first_10_prime_num.py
+++ first_10_prime_num.py
@@ -1,1 +1,25 @@
+def main():
+    n = int(input('Enter the value of n->'))
+    print_prime_numbers(n)
 
+def print_prime_numbers(n):
+    num = 2
+    while n!= 0:
+        if is_prime(num) is True:
+            print(f'{num =} is a prime number')
+            n = sum((n, -1))
+        num = sum((num,1)) 
+
+def is_prime(num):
+    if count_of_factors(num) == 2:
+        return True 
+    return False
+
+def count_of_factors(num):
+    res = 0
+    for n in range(1,num +1):
+        if num % n == 0:
+            res = sum((res,1)) 
+    return res
+
+main()                             
